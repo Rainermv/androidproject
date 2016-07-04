63212bee679975d7dd61f039373fbd07fbba0cb4
Rainer Vieira <rainermv@gmail.com>
2016-07-04 09:48:51 -0300

    Implementação da classe StaticGameObject

--- core/src/com/rainer/veganrevenge/GameObject.java
+++ core/src/com/rainer/veganrevenge/GameObject.java
@@ -14,18 +14,11 @@ import com.badlogic.gdx.utils.Disposable;
  */
 public class GameObject implements Disposable {
 
-    //public String sprite_file = "sprites/bg.png";
 
-    //protected Texture texture;
-    protected Sprite sprite;
     protected Rectangle rect; // remover
 
-    public GameObject(String sprite_path, Rectangle rect){
-
+    public GameObject(Rectangle rect){
         this.rect = rect;
-        Texture texture = new Texture(Gdx.files.internal(sprite_path));
-        this.sprite = new Sprite(texture, 30,30);
-
     }
 
     public GameObject(){
@@ -34,29 +27,21 @@ public class GameObject implements Disposable {
         this.rect.y = 0;
         this.rect.width = 32;
         this.rect.height = 32;
-        //this.texture = new Texture(Gdx.files.internal("sprites/bg.png"));
     }
 
-    public void setPosition(float x,float y){
-        this.sprite.setX(x);
-        this.sprite.setY(y);
-    }
+
 
     public void draw(SpriteBatch batch){
-
-        this.sprite.draw(batch);
-        //batch.draw(this.texture, this.rect.x, this.rect.y );
-
+        // VIRTUAL
     }
 
     public void onScreenTouch(Vector3 touch_position){
-
-
+        // VIRTUAL
     }
 
     @Override
     public void dispose() {
-        //sprite.dispose();
+        // VIRTUAL
     }
 
 }
---
+++ core/src/com/rainer/veganrevenge/StaticGameObject.java
@@ -0,0 +1,54 @@
+package com.rainer.veganrevenge;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Disposable;
+
+/**
+ * Created by Rainer on 04/07/2016.
+ */
+public class StaticGameObject extends GameObject{
+
+    protected Sprite sprite;
+
+    public StaticGameObject(Rectangle rect, Sprite sprite){
+
+        super(rect);
+        this.sprite = sprite;
+
+        adjustSprite();
+        //Texture texture = new Texture(Gdx.files.internal(sprite_path));
+        //this.sprite = new Sprite(texture, 30,30);
+    }
+
+    // default constructor
+    public StaticGameObject(){
+
+        super();
+
+        Texture texture = new Texture(Gdx.files.internal("sprites/medium.png"));
+        this.sprite = new Sprite(texture);
+
+        adjustSprite();
+    }
+
+    public void adjustSprite(){
+        this.sprite.setX(rect.x);
+        this.sprite.setY(rect.y);
+        this.sprite.setSize(rect.width,rect.height);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        this.sprite.draw(batch);
+    }
+
+    @Override
+    public void dispose() {
+        this.sprite.getTexture().dispose();
+    }
+
+}
